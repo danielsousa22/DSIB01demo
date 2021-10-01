d9def535969a9ea45b748343c11741fb2858f143
Daniel Sousa <daniel.p.sousa22@gmail.com>
2021-10-01 12:43:21 +0200

    Script for downloading data TAF15

---
+++ scripts/download.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#TODO: add description
+
+BASE_URL=https://www.encodeproject.org/files
+
+#TODO: insert names of your files
+FILES=(ENCFF456KCB ENCFF718IXR ENCFF185GGV ENCFF192XSY)
+
+mkdir data
+cd data
+
+for file in $FILES
+do
+	# creating url of file
+	url=$BASE_URL/$file/@@download/$file.fastq.gz
+	echo File URL: $url
+
+	#downloading file
+	wget $url
+	
+	#unzip downloaded file. zipped version is automaticaly deleted.
+	gunzip $file.fastq.gz
+done
+
+echo Downloaded files:
+ls
